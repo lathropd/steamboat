c244c22debb07404970272ec7aa5f6a99e59f800
Daniel Lathrop <daniel.lathrop@gmail.com>
2020-06-25 12:40:39 -0500

    get list of relevant geographies

--- data/create-routt-and-moffat-geographies.sh
+++ data/create-routt-and-moffat-geographies.sh
@@ -1,9 +1,10 @@
-# merge the colorado geography headers with the data
-cat geography-headers.csv g20185co.csv > colorado-geography.csv
+# merge the colorado geography headers with the datail
+# change the incosistency between the data dump and api census geoids
+cat geography-headers.csv g20185co.csv |iconv -f latin1 -t utf-8|sed -e  "s/00US/0000US/g" > colorado-geography.csv
 
 
 # create a list of geographies containing Routt, Moffat, Steamboat or Hayden School
-cat colorado-geography.csv|iconv -f latin1 -t utf-8|csvcut -v  -c 2,3,49,50|egrep -i "STUSAB|Steamboat|Routt|Moffat|Hayden School" > routt-and-moffat-geographies.csv
+cat colorado-geography.csv|csvcut -v  -c 2,3,49,50|egrep -i "STUSAB|\"Colorado|Steamboat|Routt|Moffat|Hayden School" > routt-and-moffat-geographies.csv
 
 # convert that to a comma-delimited list of geoids for querying the census APIs
 cat routt-and-moffat-geographies.csv | tail -n +2 | csvcut -c 3|tr "\n" , | sed -e 's/,$//' > routt-and-moffat-geographies-querystring.txt
